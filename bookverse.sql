16e15ee807f7fd1b093b0abf41f2d393774d2914
Zahab Jahangir <167731882+zeejaycoding@users.noreply.github.com>
2024-09-28 01:07:03 +0500

    Delete bookverse.sql

--- bookverse.sql
+++
@@ -1,8 +0,0 @@
-create table Books(book_id varchar (15),book_title varchar(500),author_name varchar(500),
-author_id varchar(500),genre varchar(500),publish_date date,ratings real);
-select *from books;
-create table Authors(author_id varchar(500) primary key,author_name varchar(500) not null);
-drop table authors;
-select * from authors;
-INSERT INTO Authors (author_id, author_name) SELECT DISTINCT author_id, author_name FROM books;
-SELECT DISTINCT author_name FROM books;
