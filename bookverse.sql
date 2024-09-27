f4d45ffa1d932699d029529a5e5b2da60701aef9
Zahab Jahangir <167731882+zeejaycoding@users.noreply.github.com>
2024-09-27 20:11:01 +0500

    bookverse.sql

--- bookverse.sql
+++ bookverse.sql
@@ -1,5 +1,8 @@
-create table Authors(author_id varchar(20) primary key,author_name varchar(75) not null);
-select * from authors;
 create table Books(book_id varchar (15),book_title varchar(500),author_name varchar(500),
 author_id varchar(500),genre varchar(500),publish_date date,ratings real);
 select *from books;
+create table Authors(author_id varchar(500) primary key,author_name varchar(500) not null);
+drop table authors;
+select * from authors;
+INSERT INTO Authors (author_id, author_name) SELECT DISTINCT author_id, author_name FROM books;
+SELECT DISTINCT author_name FROM books;
