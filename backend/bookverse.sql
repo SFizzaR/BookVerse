e7042920dc04716fb3386b5dca3fcdd89301c73e
Zahab Jahangir <167731882+zeejaycoding@users.noreply.github.com>
2024-09-28 21:00:59 +0500

    Update bookverse.sql

--- backend/bookverse.sql
+++ backend/bookverse.sql
@@ -3,10 +3,19 @@ author_id varchar(500),genre varchar(500),publish_date date,ratings real);
 select *from books;
 
 create table Authors(author_id varchar(500) primary key,author_name varchar(500) not null);
-drop table authors;
-select * from authors;
-INSERT INTO Authors (author_id, author_name) SELECT DISTINCT author_id, author_name FROM books;
+MERGE INTO Authors a
+USING (SELECT DISTINCT author_id, author_name FROM books) b
+ON (a.author_id = b.author_id)
+WHEN NOT MATCHED THEN
+    INSERT (author_id, author_name) 
+    VALUES (b.author_id, b.author_name);
 SELECT DISTINCT author_name FROM books;
+delete
+FROM authors
+WHERE author_name LIKE '%,%';
+select * from authors;
+
+
 
 create sequence user_idd_seq START WITH 1 INCREMENT BY 1;
 
