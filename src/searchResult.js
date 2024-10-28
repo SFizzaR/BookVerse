54e73949fb6d4deffd318dda9b8b5204d1f4231e
Zahab Jahangir <167731882+zeejaycoding@users.noreply.github.com>
2024-10-28 06:59:36 +0500

    Update searchResult.js

--- src/searchResult.js
+++ src/searchResult.js
@@ -1,6 +1,6 @@
-// SearchResults.js
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import './components/section.css'; // Ensure this path is correct
 
 function SearchResults() {
   const location = useLocation();
@@ -16,23 +16,31 @@ function SearchResults() {
         <h2 id="title">Book Verse</h2>
       </div>
       <div id='display-results'>
-      <h2>Search Results</h2>
-      <p>Search Type: {searchType}</p>
-      <p>Search Term: {searchTerm}</p>
+        <h2>Search Results</h2>
+        <p>Search Type: {searchType}</p>
+        <p>Search Term: {searchTerm}</p>
 
-      <div>
-        {searchResults && searchResults.length > 0 ? (
-          searchResults.map((result, index) => (
-            <div key={index}>
-              <h3>{result.title}</h3>
-              <p>Author: {result.author}</p>
-              <p>Found what you were looking for?<span onClick={handleBack} style={{ cursor: 'pointer', color: 'blue', textDecoration: 'underline' }}>Click here</span> to search again.</p>
-            </div>
-          ))
-        ) : (
-          <p>No books found matching your search criteria.<span onClick={handleBack} style={{ cursor: 'pointer', color: 'blue', textDecoration: 'underline' }}>Click here</span> to try again</p>
-        )}
-      </div>
+        <div className="book-list">
+          {searchResults && searchResults.length > 0 ? (
+            searchResults.map((result, index) => (
+              <div key={index} className="book-card">
+                <img src={result.bookImageUrl} alt={result.title} className="book-cover" />
+                <h3>{result.title}</h3>
+                <p>Author: {result.author}</p>
+                <p>Rating: {result.ratings} ⭐</p>
+              
+              </div>
+            ))
+          ) : (
+            <p>No books found matching your search criteria.
+              <span 
+                onClick={handleBack} 
+                style={{ cursor: 'pointer', color: 'blue', textDecoration: 'underline' }}>
+                Click here
+              </span> to try again
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
